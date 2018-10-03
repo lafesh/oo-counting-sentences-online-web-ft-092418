0306af36da286972241ce5e534ac7e99cd346e75
Gina Fedder <ginafedder@yahoo.com>
2018-10-03 15:36:09 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,10 +27,10 @@ class String
   end
 
   def count_sentences
-
+<<<<<<< HEAD
     self.split(/[?.!]+/).count
-
+=======
     self.split(/\W/).count
-
+>>>>>>> 3ff27ea2d1a7a7d28174e4d4b7144a14ed08acb8
   end
 end
